4edf795f65881ee7d8662346d893ab37dd52a352
Brad Chiappetta <brad@greynoise.io>
2021-02-24 09:59:27 -0500

    fix linter errors

--- greynoise/sw_greynoise/__init__.py
+++ greynoise/sw_greynoise/__init__.py
@@ -1,11 +1,12 @@
 from greynoise import GreyNoise
 
+PLUGIN_VERSION = "v1.0.0"
 
-PLUGIN_VERSION = 'v1.0.0'
 
 class GreynoiseBaseClass(object):
     def __init__(self, context):
         self.api_key = context.asset["api_key"]
         self.session = GreyNoise(
-            api_key=self.api_key, integration_name="greynoise-swimlane-" + PLUGIN_VERSION
+            api_key=self.api_key,
+            integration_name="greynoise-swimlane-" + PLUGIN_VERSION,
         )
