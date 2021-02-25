6acbcc2f611ea94ec737b2319ba68f333231be7c
Brad Chiappetta <brad@greynoise.io>
2021-02-25 13:00:04 -0500

    update from review

--- greynoise/CHANGELOG.md
+++ greynoise/CHANGELOG.md
@@ -1,6 +1,6 @@
 # CHANGELOG
 
-## 1.0.0 - 2021-02-22
+## 1.0.0 - 2021-02-25
 
 * Initial Release
 * Product version tested against: 10.2.2
--- greynoise/imports/test_connection.py
+++ greynoise/imports/test_connection.py
@@ -1,13 +1,12 @@
+from swimbundle_utils.helpers import create_test_conn
 from sw_greynoise import GreynoiseBaseClass
 
 
-class SwMain(GreynoiseBaseClass):
-    def execute(self):
+class Test(GreynoiseBaseClass):
+    def connect(self):
         response = str(self.session.test_connection())
         if response != "Success: Access and API Key Valid":
-            return {
-                "successful": False,
-                "errorMessage": "API returned Code: " + str(response),
-            }
-        else:
-            return {"successful": True}
+            raise Exception("Connection Error: " + response)
+
+
+SwMain = create_test_conn(Test, "connect")
